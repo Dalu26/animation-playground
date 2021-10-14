a565471aa6faa278b7df930ec4a43f5147c47da4
@Dalu26 <ikeanyichukwudalu@gmail.com>
2021-10-14 09:11:52 +0100

    updates

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ import React from 'react';
 import { enableScreens } from 'react-native-screens';
 import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 import { NavigationContainer } from '@react-navigation/native';
-import List from './src/transition_screens/List'
-import Details from './src/transition_screens/Details'
+import List from './src/screens/transition_screens/List'
+import Details from './src/screens/transition_screens/Details'
 enableScreens()
 
 const Stack = createSharedElementStackNavigator()
