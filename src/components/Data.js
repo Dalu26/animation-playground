2096e1fdd6354771e382830f8e86774bdc687a16
@Dalu26 <ikeanyichukwudalu@gmail.com>
2021-10-14 09:22:58 +0100

    updates

---
+++ src/components/Data.js
@@ -0,0 +1,29 @@
+const Data = [
+    {
+    imageUri:"https://image.flaticon.com/tcons/png/128/2990/2990502.png",
+    title: "Ukelele",
+    id: "2990502",
+    },
+    {
+    imageUri: 'https://image.flaticon.com/icons/png/128/2990/2990505.png',
+    title: "Sea",
+    id: "2990505",
+    },
+    {
+    imageUri: 'https://image.flaticon.con/tcons/pna/128/2990/2990508.png',
+    title: "Tikt",
+    id: "2990508",
+    },
+    {
+    imageUri: "https://image.flaticon.com/Lcons/pna/128/2990/2990510.png",
+    title: "Banana",
+    id: "2990510",
+    },
+    {
+    imageUri:'https://image.flaticon.com/icons/png/128/2990/2990512.png',
+    title: "Coconut tree",
+    id: "2998512",
+    }
+]
+
+export default Data
--- src/screens/transition_screens/List.js
+++ src/screens/transition_screens/List.js
@@ -1,41 +1,15 @@
 import React from 'react'
 import { StyleSheet, View, SafeAreaView, Text, TouchableOpacity } from 'react-native'
+import Data from '../../components/Data'
 
 const List = () => {
     const { container } = styles
-    const DATA = [
-        {
-        imageUri:"https://image.flaticon.com/tcons/png/128/2990/2990502.png",
-        title: "Ukelele",
-        id: "2990502",
-        },
-        {
-        imageUri: 'https://image.flaticon.com/icons/png/128/2990/2990505.png',
-        title: "Sea",
-        id: "2990505",
-        },
-        {
-        imageUri: 'https://image.flaticon.con/tcons/pna/128/2990/2990508.png',
-        title: "Tikt",
-        id: "2990508",
-        },
-        {
-        imageUri: "https://image.flaticon.com/Lcons/pna/128/2990/2990510.png",
-        title: "Banana",
-        id: "2990510",
-        },
-        {
-        imageUri:'https://image.flaticon.com/icons/png/128/2990/2990512.png',
-        title: "Coconut tree",
-        id: "2998512",
-        }
-    ]
 
     return(
         <SafeAreaView style={container}>
             <View>
                 <Text>Animations List Screen</Text>
-                {DATA.map((item) => {
+                {Data.map((item) => {
                     return(
                         <TouchableOpacity key={item.id} style={{}} onPress={() => {}} >
                         {/* Icon uni=(item.imageUri)/> */}
